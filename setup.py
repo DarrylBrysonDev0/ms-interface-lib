019db3c5124815c5a04070f8d926633dfdb0af51
DarrylBryson <62783148+DarrylBrysonDev0@users.noreply.github.com>
2021-03-01 20:02:00 -0500

    init project

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="microsrv-interface-darryl.bryson", 
+    version="0.0.1",
+    author="Darryl Bryson",
+    author_email="Darryl.Bryson@no-reply.com",
+    description="Custom interface classes for microservice communication",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DarrylBrysonDev0/ms-interface-lib.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/DarrylBrysonDev0/ms-interface-lib/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
